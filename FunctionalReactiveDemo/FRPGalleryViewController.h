b85f983767aab25a5ed7fa83c7fc8995a8ae6a36
陈宇峰 <yufengcode@gmail.com>
2016-05-23 15:57:26 +0800

    init

--- FunctionalReactiveDemo/FRPGalleryViewController.h
+++ FunctionalReactiveDemo/FRPGalleryViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  FRPGalleryViewController.h
 //  FunctionalReactiveDemo
 //
 //  Created by chenyufeng on 16/5/23.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface FRPGalleryViewController : UICollectionViewController
 
 @end
-

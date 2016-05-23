ff3924daa74c617d19865510401fa9525c195882
陈宇峰 <yufengcode@gmail.com>
2016-05-23 16:28:15 +0800

    pod install

--- Podfile
+++ Podfile
@@ -8,7 +8,3 @@ pod '500px-iOS-api', '1.0.4'
 pod 'SVProgressHUD', '0.9'
 
 end
-
-target "FunctionalReactiveDemoTests" do
-
-end
---
+++ Pods/Target Support Files/libffi/libffi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_libffi : NSObject
+@end
+@implementation PodsDummy_libffi
+@end
